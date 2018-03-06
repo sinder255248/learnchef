a5e381902fc42adb4de3f77d1beea252e81525a7
sinder255248 <brian_adkins@btinternet.com>
2018-03-06 12:52:17 +0000

    removing knife.rb

--- knife.rb
+++ knife.rb
@@ -4,7 +4,7 @@ log_location             STDOUT
 node_name                "admin"
 client_key               "c:\chef-repo\.chef\admin.pem"
 validation_client_name   "barclays-validator"
-validation_key           "c:\chef-repo\.chef\\barclays-validator.pem"
+validation_key           "c:\chef-repo\.chef\barclays-validator.pem"
 chef_server_url          "https://chef12server.barclays.local/organizations/barclays"
 syntax_check_cache_path  "#{ENV['HOME']}\.chef\syntaxcache"
 cookbook_path            ["c:\chef-repo\cookbooks"]
