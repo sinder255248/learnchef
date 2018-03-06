3770baccfc1bc519c603e542e078cb75d60570ba
sinder255248 <brian_adkins@btinternet.com>
2018-03-06 12:44:02 +0000

    updating knife.rb

--- knife.rb
+++ knife.rb
@@ -2,9 +2,9 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "admin"
-client_key               "#{current_dir}\admin.pem"
+client_key               "c:\chef-repo\.chef\admin.pem"
 validation_client_name   "barclays-validator"
-validation_key           "#{current_dir}\barclays-validator.pem"
+validation_key           "c:\chef-repo\.chef\\barclays-validator.pem"
 chef_server_url          "https://chef12server.barclays.local/organizations/barclays"
 syntax_check_cache_path  "#{ENV['HOME']}\.chef\syntaxcache"
-cookbook_path            ["#{current_dir}\..\cookbooks"]
+cookbook_path            ["c:\chef-repo\cookbooks"]
